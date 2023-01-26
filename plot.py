432db100d03b1140ba089869081e405918c93104
rishab26201 <rishab.edu@gmail.com>
2023-01-25 22:17:49 -0800

    Final plots

--- plot.py
+++ plot.py
@@ -5,17 +5,18 @@ import matplotlib.colors as mcolors
 import os
 import argparse
 
-SMALL_SIZE = 8
-MEDIUM_SIZE = 12
-BIGGER_SIZE = 15
+SMALL_SIZE = 18
+MEDIUM_SIZE = 15
+BIGGER_SIZE = 18
 
 plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+
 
 # plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
-# plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
-# plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
 # plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 parser = argparse.ArgumentParser()
@@ -90,7 +91,7 @@ plt.title(title)
 t = ax.yaxis.get_offset_text()
 t.set_x(-0.05)
 
-# plt.tight_layout()
+plt.tight_layout()
 
 print("saving")
 plt.savefig(os.path.join('./SimulationResults', args.exp_name, args.exp_type + '.pdf'))
